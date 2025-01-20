10a6fc71bb9d13a43ac5d07da4e9cf714c39ab11
guhan-balaji <guhanbalaji7@gmail.com>
2025-01-20 13:38:15 -0500

    read a blob

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+        "bytes"
+        "compress/zlib"
 	"fmt"
+        "io"
 	"os"
+        "strings"
 )
 
 // Usage: your_program.sh <command> <arg1> <arg2> ...
@@ -28,6 +32,40 @@ func main() {
 		}
 		
 		fmt.Println("Initialized git directory")
+
+        case "cat-file":
+                if os.Args[2] != "-p" {
+			fmt.Fprintf(os.Stderr, "Invalid flag: %s\n", os.Args[2])
+                }
+
+                hash := os.Args[3]
+                if len(hash) != 40 {
+			fmt.Fprintf(os.Stderr, "Invalid hash: %s\n", hash)
+                }
+                
+                dir, file := hash[:2], hash[2:]
+                b, err := os.ReadFile("./.git/objects/" + dir + "/" + file)
+                if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading blob: %s\n", err)
+                }
+
+                buff := bytes.NewBuffer(b)
+                r, err := zlib.NewReader(buff)
+                if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading blob: %s\n", err)
+                }
+                defer r.Close()
+
+                var sb strings.Builder
+                io.Copy(&sb, r)
+                blob := sb.String()
+                for i, c := range blob {
+                        if c == 0 {
+                                content := blob[i+1:]
+                                fmt.Print(content)
+                                break
+                        }
+                }
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
