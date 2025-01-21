d4c587c27fa6c11f6f561da8c817fbc74e1345f6
guhan-balaji <guhanbalaji7@gmail.com>
2025-01-20 19:11:18 -0500

    create blob object

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -3,6 +3,7 @@ package main
 import (
         "bytes"
         "compress/zlib"
+        "crypto/sha1"
 	"fmt"
         "io"
 	"os"
@@ -33,39 +34,87 @@ func main() {
 		fmt.Println("Initialized git directory")
 
         case "cat-file":
-                if os.Args[2] != "-p" {
-			fmt.Fprintf(os.Stderr, "Invalid flag: %s\n", os.Args[2])
-                }
-
-                hash := os.Args[3]
-                if len(hash) != 40 {
-			fmt.Fprintf(os.Stderr, "Invalid hash: %s\n", hash)
-                }
-                
-                dir, file := hash[:2], hash[2:]
-                b, err := os.ReadFile("./.git/objects/" + dir + "/" + file)
-                if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading blob: %s\n", err)
-                }
-
-                buff := bytes.NewBuffer(b)
-                r, err := zlib.NewReader(buff)
-                if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading blob: %s\n", err)
-                }
-                defer r.Close()
-
-                blob, err := io.ReadAll(r)
-                if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading blob: %s\n", err)
-                }
-
-                parts := bytes.Split(blob, []byte("\x00"))
-                content := string(parts[1])
-                fmt.Print(content)
+                handleCatFile()
+        
+        case "hash-object":
+                handleHashObject()
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
 	}
+}
+
+func handleCatFile() {
+        if os.Args[2] != "-p" {
+                fmt.Fprintf(os.Stderr, "Invalid flag: %s\n", os.Args[2])
+		os.Exit(1)
+        }
+
+        hash := os.Args[3]
+        if len(hash) != 40 {
+                fmt.Fprintf(os.Stderr, "Invalid hash: %s\n", hash)
+		os.Exit(1)
+        }
+
+        dir, file := hash[:2], hash[2:]
+        b, err := os.ReadFile("./.git/objects/" + dir + "/" + file)
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "Error reading blob: %s\n", err)
+		os.Exit(1)
+        }
+
+        buff := bytes.NewBuffer(b)
+        r, err := zlib.NewReader(buff)
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "Error reading blob: %s\n", err)
+		os.Exit(1)
+        }
+        defer r.Close()
+
+        blob, err := io.ReadAll(r)
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "Error reading blob: %s\n", err)
+		os.Exit(1)
+        }
+
+        parts := bytes.Split(blob, []byte("\x00"))
+        content := string(parts[1])
+        fmt.Print(content)
+}
+
+func handleHashObject() {
+        fn := os.Args[3]
+        b, err := os.ReadFile(fn)
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "Error reading file %s: %s\n", fn, err)
+		os.Exit(1)
+        }
+        s := string(b)
+        content := fmt.Sprintf("blob %d\x00%s", len(s), s)
+
+        h := sha1.New()
+        h.Write([]byte(content))
+        hash := fmt.Sprintf("%x", h.Sum(nil))
+        fmt.Println(hash)
+
+        if os.Args[2] == "-w" {
+                fn = fmt.Sprintf("./.git/objects/%s/%s", hash[:2], hash[2:])
+                err = os.MkdirAll("./.git/objects/" + hash[:2], 0755)
+                if err != nil {
+                        fmt.Fprintf(os.Stderr, "Error writing object file %s: %s\n", fn, err)
+                        os.Exit(1)
+                }
+
+                var b bytes.Buffer
+                w := zlib.NewWriter(&b)
+                w.Write([]byte(content))
+                w.Close()
+
+                err = os.WriteFile(fn, b.Bytes(), 0755)
+                if err != nil {
+                        fmt.Fprintf(os.Stderr, "Error writing object file %s: %s\n", fn, err)
+                        os.Exit(1)
+                }
+        }
 }
